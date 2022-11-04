d9ba0fb7e0af1c2c628c935503316cdb61f99396
ValDelaCruz <116246965+ValDelaCruz@users.noreply.github.com>
2022-11-04 13:35:07 +0700

    Add files via upload

---
+++ FrmBasicThread.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace DelaCruzThreads
+{
+    partial class FrmBasicThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lblThread = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.Color.Tan;
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(198, 205);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(122, 39);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lblThread
+            // 
+            this.lblThread.AutoSize = true;
+            this.lblThread.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThread.Location = new System.Drawing.Point(74, 69);
+            this.lblThread.Name = "lblThread";
+            this.lblThread.Size = new System.Drawing.Size(352, 38);
+            this.lblThread.TabIndex = 1;
+            this.lblThread.Text = "-Before starting thread-";
+            this.lblThread.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmBasicThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(530, 275);
+            this.Controls.Add(this.lblThread);
+            this.Controls.Add(this.btnRun);
+            this.Name = "FrmBasicThread";
+            this.Text = "BasicThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label lblThread;
+    }
+}
+
---
+++ FrmBasicThread.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelaCruzThreads
+{
+    public partial class FrmBasicThread : Form
+    {
+        private Thread ThreadA, ThreadB;
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            ThreadStart threadObj = new ThreadStart(MyThreadClass.Thread1);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Thread.Sleep(2000);
+            Console.WriteLine("Hi, I'm Allysah Valerie C. Dela Cruz");
+            Thread.Sleep(1500);
+            Console.WriteLine("From BSIT 3.1 - A");
+            Thread.Sleep(1000);
+            Console.WriteLine(".");
+            Thread.Sleep(1000);
+            Console.WriteLine(".");
+            Thread.Sleep(1000);
+            Console.WriteLine(".");
+            Thread.Sleep(1000);
+            Console.WriteLine("-Before starting thread-");
+            Thread.Sleep(1000);
+
+            ThreadA = new Thread(threadObj);
+            ThreadA.Name = "Thread A Process";
+            ThreadB = new Thread(threadObj);
+            ThreadB.Name = "Thread B Process";
+
+            //Thread starting...
+            ThreadA.Start();
+            ThreadB.Start();
+
+            //Thread calling stopped...
+            ThreadA.Join();
+            ThreadB.Join();
+
+            Console.WriteLine("-End of Thread-");
+            lblThread.Text = "-End of Thread-";            
+        }
+
+        
+    }
+}
